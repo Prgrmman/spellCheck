876ae56f6cb04936ea50b9a4b2ebd75b63be1fd9
jon <jon.programming@gmail.com>
2016-11-29 12:20:59 -0500

    fixed spacing

--- dictOperations/dictOperations.hpp
+++ dictOperations/dictOperations.hpp
@@ -4,7 +4,7 @@
  *  --> use createDict(std::string filename) to   *
  *      Create a dictionary of words to be used for finding words within 2 edit distance       *
  ********************************************************************/
- #ifndef DICTOPERATIONS_H
+#ifndef DICTOPERATIONS_H
 #define DICTOPERATIONS_H
 #include <string>
 
