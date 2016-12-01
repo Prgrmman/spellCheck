2594ea398d45bd104c7f49ec3472e2bc33be4a75
jon <jon.programming@gmail.com>
2016-12-01 15:50:33 -0500

    demo is better

--- main.cpp
+++ main.cpp
@@ -6,28 +6,19 @@ using namespace std;
 int main(int argc, char** argv)
 {
     string correction;
-	string wordToCorrect = argv[1];
-	string dictName = argv[2];
-	createDict(dictName);
-	correction = findCorrection(wordToCorrect);
-	cout << correction << endl;
-	
-	/*string correction;
-	cout << "Testing" << endl;
-    list<string> words = getWords(filename);
-    Text text(words);
-    cout << "freq of " << argv[1] <<" " << text.getProportion(argv[1]) << endl;
-    // test edit distance
-    string str1 = argv[1]; 
-    string str2 = argv[2]; 
-    cout << "Edit distance is " << editDistance(str1,str2) << endl;
-	string dictName = argv[3];
-	//cout << "Current dictionary contents:" << endl;
-	createDict(dictName);
-	correction = findCorrection("aaron");
-	cout << correction << endl;
-	correction = findCorrection("aaab");
-	cout << correction << endl;*/
-
+    string wordToCorrect = argv[1];
+    string dictName = argv[2];
+    createDict(dictName);
+    correction = findCorrection(wordToCorrect);
+    cout << correction << endl;
+    string word;
+    while(true){
+        cout << "Word: ";
+        cin >> word;
+        cout << endl;
+        correction = findCorrection(word);
+        cout << correction << endl;
+        
+    }
     return 0;
 }
